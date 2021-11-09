3ee5690cc2d178305426dbede231f87690ad790d
Emily Ramond <72273345+emily-ramond@users.noreply.github.com>
2021-11-09 11:10:50 -0800

    Added comments to download_data.py
    We need to add variables to the function to ensure we aren't hardcoding any numbers, this might not be needed since it is a replication.

--- src/scripts/Download_data.py
+++ src/scripts/Download_data.py
@@ -1,3 +1,4 @@
+# IMPORT STATEMENTS
 import netCDF4
 import numpy as np
 import requests
@@ -6,27 +7,28 @@ import os
 import logging
 from pathlib import Path
 
-
+# FUNCTION TO DOWNLOAD DATA 
 def download_file(year):
     try:
-        file_name = f'hgt.{year}.nc'
+        file_name = f'hgt.{year}.nc' # grabs file names in hgt.year format
         cwd = os.getcwd()
         file_path = f'{cwd}/src/data/{file_name}'
+        
         if Path(file_path).exists():
-            logging.info(f'Data for {year} is already downloaded.')
+            logging.info(f'Data for {year} is already downloaded.') #sends logging info to log file
         else:
-            url = 'https://downloads.psl.noaa.gov/Datasets/ncep.reanalysis.dailyavgs/pressure/'
-            file_url = f'{url}{file_name}'
-            file = requests.get(file_url)
+            url = 'https://downloads.psl.noaa.gov/Datasets/ncep.reanalysis.dailyavgs/pressure/' #location of file  
+            file_url = f'{url}{file_name}' 
+            file = requests.get(file_url) #requests the file 
             
             
-            open(f'src/data/{file_name}','wb').write(file.content)
-            logging.info(f'Dara for {year} successfully downloaded.')
+            open(f'src/data/{file_name}','wb').write(file.content) #writes the file content 
+            logging.info(f'Dara for {year} successfully downloaded.') #reports successful file download 
 
     except:
-        logging.error('Unexpected error while downloading data from {year}.')
+        logging.error('Unexpected error while downloading data from {year}.') #log any errors 
 
-
+#downloads the files for each year ***NEED TO ADD VARIABLES FOR EASY USE IN FUTURE***
 for i in range(11):
     yr = 2011 + i
     download_file(yr)
