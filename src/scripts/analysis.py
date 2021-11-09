66dca08d135f39bded4aded67ba734692adf6987
Emily Ramond <72273345+emily-ramond@users.noreply.github.com>
2021-11-09 11:32:31 -0800

    Update analysis.py

--- src/scripts/analysis.py
+++ src/scripts/analysis.py
@@ -1,3 +1,4 @@
+#IMPORT STATEMENTS 
 import netCDF4
 import numpy as np
 import pandas as pd
@@ -9,14 +10,17 @@ from pathlib import Path
 import xarray as xr
 
 
-
+#function to read the file 
 def read_nc_file(year):
     try:
         #reads the file 
         file_name = f'hgt.{year}.nc'
         cwd = os.getcwd()
+        #gets file path
         file_path = f'{cwd}/src/data/{file_name}'
+        #uses xarray to open the file 
         ds = xr.open_dataset(file_path)
+        #turns the read in file to a dataframe 
         df = ds.to_dataframe()
         print(df.head())
         f = netCDF4.Dataset(file_path)
